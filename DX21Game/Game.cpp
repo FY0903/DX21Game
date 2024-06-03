303b0c4f55bf3cd54bc3c77ddc496cb5e06f5aac
FY0903 <158162451+FY0903@users.noreply.github.com>
2024-06-03 09:26:12 +0900

    0603 9:25 授業内容

--- DX21Game/Game.cpp
+++ DX21Game/Game.cpp
@@ -26,18 +26,17 @@ bool InitGame(HWND hWnd)
 
 	// 頂点バッファの作成
 	Vertex vtx[] = { 
-		{{-100.0f, -100.0f, 0.0f}, {0.0f, 0.0f}}, 
-		{{-100.0f, 100.0f, 0.0f}, {0.0f, 1.0f}}, 
-		{{100.0f, -100.0f, 0.0f}, {1.0f, 0.0f}}, 
-		{{100.0f, 100.0f, 0.0f}, {1.0f, 1.0f}} };
+		{{-400.0f, -400.0f, 0.0f}, {0.0f, 0.0f}},
+		{{-400.0f, 400.0f , 0.0f}, {0.0f, 1.0f}},
+		{{400.0f, -400.0f, 0.0f}, {1.0f, 0.0f}},
+		{{400.0f, 400.0f, 0.0f}, {1.0f, 1.0f}} };
 	g_pVtxBuf = CreateVertexBuffer(vtx, 4);
 
-	if (FAILED(LoadTextureFromFile(GetDevice(), "test.jpg", &g_pTex)))
+	if (FAILED(LoadTextureFromFile(GetDevice(), "Visual_Studio_Icon_2022.png", &g_pTex)))
 	{
 		MessageBox(hWnd, "テクスチャー読み込み失敗", "エラー", S_OK);
 		return false;
 	}
-	SetSpriteTexture(g_pTex);
 
 	return true;
 }
@@ -56,6 +55,7 @@ void UpdateGame()
 void DrawGame()
 {
 	BeginDrawDirectX();
+	SetSpriteTexture(g_pTex);
 	DrawSprite(g_pVtxBuf, sizeof(Vertex));
 	EndDrawDirectX();
 }
