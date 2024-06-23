56e853b5deb1f34b4e3d8e55da94618b9134a407
FY0903 <xxxxbgmxxxx@gmail.com>
2024-06-23 23:39:57 +0900

    06/23 23:39 scenemanager中身完成

--- DX21Game/scene.h
+++ DX21Game/scene.h
@@ -2,12 +2,49 @@
 
 class CManager;
 
+/**
+ * @brief シーンの基底クラス
+ */
 class CScene
 {
+protected:
+	CManager* m_pManager;
 
+public:
+	CScene(CManager* pManager) : m_pManager(pManager) {}
+	virtual ~CScene() {}
+	
+	/**
+	 * @brief 更新
+	 */
+	virtual void Update() = 0;
+
+	/**
+	 * @brief 描画
+	 */
+	virtual void Draw() = 0;
 };
 
+/**
+ * @brief シーン管理クラス
+ */
 class CManager
 {
+public:
+	/**
+	 * @brief 現在シーンのポインタ
+	 */
+	CScene* m_pScene;
 
+	~CManager() { delete m_pScene; }
+
+	/**
+	 * @brief 更新
+	 */
+	void Update() { m_pScene->Update(); }
+
+	/**
+	 * @brief 描画
+	 */
+	void Draw() { m_pScene->Draw(); }
 };
